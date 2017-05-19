cb07a57cdc3898cdb8650d5527bf9fafb8d8b1fd
crhodes21 <calebtrhodes@gmail.com>
2017-05-19 10:37:04 -0400

    final project stuff

--- Caleb/Makin' Waves.py
+++ Caleb/Makin' Waves.py
@@ -6,12 +6,13 @@ from sklearn.model_selection import train_test_split
 wisconsin_data = pd.read_csv('breast-cancer-wisconsin.csv', index_col=False, header=0).dropna('id', axis=1)
 
 
-
-
 X_train, X_test, y_train, y_test = train_test_split(features.values.astype(int), labels.values.astype(int), test_size=.33)
 clf = MLPClassifier()
 clf.fit(X_train, y_train)
 
-print clf.score(X_test, y_test)
+print
+
+
+#ask alex what score means, is there a way to correspond the predicted values with the serial numbers of the data set, is there a way I can feed the model a random attribute set and have it predict the result
 
 
--- Caleb/Workin through the answer.py
+++ Caleb/Workin through the answer.py
@@ -8,4 +8,6 @@ X_train, X_test, y_train, y_test = train_test_split(features.values.astype(int),
 clf = MLPClassifier()
 clf.fit(X_train, y_train)
 print clf.score(X_test, y_test)
+print clf.predict(X_test)
+#make a new csv file with made up inputs
 
