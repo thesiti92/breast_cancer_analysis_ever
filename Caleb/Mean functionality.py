db98ec023827ff14c89bedd821f90061d9cbf568
crhodes21 <calebtrhodes@gmail.com>
2017-05-22 14:17:03 -0400

    Tyler remains a god

--- Caleb/Mean functionality.py
+++ Caleb/Mean functionality.py
@@ -14,29 +14,45 @@ def evaluator(file,endString):
         clf.score(X_test, y_test)
         adder = adder + clf.score(X_test, y_test)
         count = count + 1
-    print("Here is the average")
-    print(str(adder/count))
-    print(endString)
+    #print("Here is the average")
+    #print(str(adder/count))
+    #print(endString)
+    return(adder/count)
 
+
+evaluator("main_cancer_data.csv","End of all attribute data")
 evaluator("clump_thickness.csv", "End of clump thickness data")
 evaluator("size_uniformity.csv","End of size uniformity data")
 evaluator("shape_uniformity.csv","End of shape uniformity data")
-evaluator()
-evaluator()
-evaluator()
-evaluator()
-evaluator()
-evaluator()
+evaluator("marginal_adhesion.csv","End of marginal adhesion data")
+evaluator("epithelial_size.csv","End of epithelial size data")
+evaluator("bare_nuclei.csv","End of bare nuclei data")
+evaluator("bland_chromatin.csv","End of bland chromatin data")
+evaluator("normal_nucleoli.csv","End of normal nucleoli data")
+evaluator("mitosis.csv","End of mitosis data")
 
+MeanArray = []
+MeanArray.append(evaluator("main_cancer_data.csv","End of all attribute data"))
+MeanArray.append(evaluator("clump_thickness.csv", "End of clump thickness data"))
+MeanArray.append(evaluator("size_uniformity.csv","End of size uniformity data"))
+MeanArray.append(evaluator("shape_uniformity.csv","End of shape uniformity data"))
+MeanArray.append(evaluator("marginal_adhesion.csv","End of marginal adhesion data"))
+MeanArray.append(evaluator("epithelial_size.csv","End of epithelial size data"))
+MeanArray.append(evaluator("bare_nuclei.csv","End of bare nuclei data"))
+MeanArray.append(evaluator("bland_chromatin.csv","End of bland chromatin data"))
+MeanArray.append(evaluator("normal_nucleoli.csv","End of normal nucleoli data"))
+MeanArray.append(evaluator("mitosis.csv","End of mitosis data"))
+print(MeanArray)
 
-
-objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
+'''
+objects = ('All Attributes', 'Clump Thickness', 'Size Uniformity', 'Shape Uniformity', 'Marginal Adhesion', 'Epithelial Size','Bare Nuclei','Bland Chromatin','Normal Nucleoli,'Mitosis')
 y_pos = np.arange(len(objects))
-performance = [10, 8, 6, 4, 2, 1]
+MeanArray = []
 
-plt.bar(y_pos, performance, align='center', alpha=0.5)
+plt.bar(y_pos, MeanArray, align='center', alpha=0.5)
 plt.xticks(y_pos, objects)
-plt.ylabel('Usage')
-plt.title('Programming language usage')
+plt.ylabel('Model Accuracy')
+plt.xlabel('Attribute Type')
+plt.title('Attribute Correlation to Classification Accuracy')
 
 plt.show()'''
